c6a884210945c6107ad7a6de3c476ddb5ef3341a
jcninci <73916763+jcninci@users.noreply.github.com>
2020-12-01 16:56:01 -0300

    Add files via upload

---
+++ primerEjercicio/funciones.js
@@ -0,0 +1,15 @@
+var nombre = prompt("Ingrese un nombre");
+console.log("Hola "+nombre+"!");
+
+
+var resultado;
+var numero2 = 10;
+var numero = prompt("Ingrese un numero");
+numero = Number(numero);
+resultado = numero+numero2;
+console.log("El resultado es "+resultado)
+
+
+var texto  = prompt("Ingrese un texto: ");
+var texto2 = prompt("Ingrese otro texto");
+alert(texto+texto2);
