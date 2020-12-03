20caa06175c94a9fa1b673d9d4c1450b99e612ab
jcninci <73916763+jcninci@users.noreply.github.com>
2020-12-03 11:06:41 -0300

    Add files via upload

---
+++ segunda entrega/funcion.js
@@ -0,0 +1,16 @@
+let numero =prompt("Ingrese un numero");
+if(numero>1000){
+    alert("El numero es mayor a 1000")
+} else alert("El numero es menor a 1000")
+
+
+let texto  = prompt("Ingrese un texto: ");
+if(texto=="Hola"){
+    console.log("El texto dice 'Hola'");
+} else console.log("El texto no dice 'Hola'");
+
+
+let numero2=prompt("Ingrese un numero");
+if(numero2>10){
+    if(numero2<50) alert("El numero esta entra 10 y 50")
+} 
