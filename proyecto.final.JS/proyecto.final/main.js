fba5bcc660979fb781b02bff45b34f76d0586f9a
jcninci <73916763+jcninci@users.noreply.github.com>
2021-01-05 11:07:57 -0300

    Add files via upload

--- proyecto.final.JS/proyecto.final/main.js
+++ proyecto.final.JS/proyecto.final/main.js
@@ -1,14 +1,87 @@
 let carts = document.querySelectorAll('.add-cart');
 
-for (let i=0; i< carts.length; i++){
+let products = [
+    {
+        name: 'jarra escocesa',
+        tag: 'jarraEscocesa',
+        price: 3500,
+        inCart: 0
+    },
+    {
+        name: 'ensaladera gotas',
+        tag: 'ensaladeraGotas',
+        price: 3500,
+        inCart: 0
+    },
+    {
+        name: 'plato con higos',
+        tag: 'platoConHigos',
+        price: 3500,
+        inCart: 0
+    },
+    {
+        name: 'taza de frutillas',
+        tag: 'tazaDeFrutillas',
+        price: 3500,
+        inCart: 0
+    },
+];
+
+for (let i=0; i < carts.length; i++){
     carts[i].addEventListener('click',() => {
-     cartNumbers();
+     cartNumbers(products[i]);
     })
 }
 
- function cartNumbers(){
+function  onLoadCartNumbers (){
     let productNumbers = localStorage.getItem('cartNumbers');
-    console.log(productNumbers);
-    console.log(typeof productNumbers) 
-    localStorage.setItem('cartNumbers' , 1);
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+ function cartNumbers(products){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+     
+    
+    productNumbers = parseInt(productNumbers);
+    
+    if ( productNumbers) {
+        localStorage.setItem('cartNumbers' , productNumbers + 1);
+        document.querySelector ('.carrito span').textContent = productNumbers +1;
+    } else {
+        localStorage.setItem('cartNumbers' , 1);
+        document.querySelector ('.carrito span').textContent = 1; 
+    }
+
+    setItems(products);
+
  }
+
+ function setItems(products){
+    let cartItems = localStorage.getItem ('prodcutsInCart');
+    cartItems = JSON.parse(cartItems);
+    console.log ("my cartItems are" , cartItems); 
+
+    if(cartItems != null){
+        
+        if(cartItems[products.tag] ==undefined){
+            cartItems = {
+                ...cartItems,
+                [products.tag]: products
+            }
+        }
+        cartItems[products.tag].inCart += 1;
+    }
+    products.inCart = 1;
+     cartItems = {
+        [products.tag]: products 
+    }
+    
+    
+     localStorage.setItem("productsInCart" , JSON.stringify (cartItems));
+
+ }
+ 
+ onLoadCartNumbers(); 
